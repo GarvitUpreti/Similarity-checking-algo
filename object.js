b793442593066cf44d09e912eefb1d6b05382296
Garvit Upreti <139622842+GarvitUpreti@users.noreply.github.com>
2024-09-20 21:19:59 +0530

    Update object.js

--- object.js
+++ object.js
@@ -1,18 +1,20 @@
+// It is the structure of the object we need to pass in function matchAB
+
 {
-    key: number,            // Represents the musical key of the song
-    genre: string[],        // Array of genres associated with the song
-    ArtistId: number,       // Unique identifier for the artist
-    songId: number,         // Unique identifier for the song
-    mode: number,           // Mode of the song (0 = minor, 1 = major)
-    Danceability: number,   // Danceability score of the song (a number between 0.0 and 1.0)
-    energy: number,         // Energy score of the song (a number between 0.0 and 1.0)
-    loudness: number,       // Loudness of the song (in dB, typically between -60 and 0)
-    Speechiness: number,    // Speechiness score of the song (a number between 0.0 and 1.0)
-    Acousticness: number,   // Acousticness score of the song (a number between 0.0 and 1.0)
-    Instrumentalness: number,// Instrumentalness score of the song (a number between 0.0 and 1.0)
-    Valence: number,        // Valence score of the song (a number between 0.0 and 1.0, represents the musical "positiveness")
-    tempo: number,          // Tempo of the song in beats per minute (BPM)
-    Liveness: number        // Liveness score of the song (a number between 0.0 and 1.0, represents how "live" the song feels)
+    key: number,              // Represents the musical key of the song
+    genre: string[],          // Array of genres associated with the song
+    ArtistId: number,         // Unique identifier for the artist
+    songId: number,           // Unique identifier for the song
+    mode: number,             // Mode of the song (0 = minor, 1 = major)
+    Danceability: number,     // Danceability score of the song (a number between 0.0 and 1.0)
+    energy: number,           // Energy score of the song (a number between 0.0 and 1.0)
+    loudness: number,         // Loudness of the song (in dB, typically between -60 and 0)
+    Speechiness: number,      // Speechiness score of the song (a number between 0.0 and 1.0)
+    Acousticness: number,     // Acousticness score of the song (a number between 0.0 and 1.0)
+    Instrumentalness: number, // Instrumentalness score of the song (a number between 0.0 and 1.0)
+    Valence: number,          // Valence score of the song (a number between 0.0 and 1.0, represents the musical "positiveness")
+    tempo: number,            // Tempo of the song in beats per minute (BPM)
+    Liveness: number          // Liveness score of the song (a number between 0.0 and 1.0, represents how "live" the song feels)
   }
   
 
