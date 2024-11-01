276228372a8dd81c9058979e6a5b82d778e1db9d
Garvit Upreti <139622842+GarvitUpreti@users.noreply.github.com>
2024-11-01 11:46:19 +0530

    Update object.js
    changed the attribute names

--- object.js
+++ object.js
@@ -3,18 +3,18 @@
 {
     key: number,              // Represents the musical key of the song
     genre: string[],          // Array of genres associated with the song
-    ArtistId: number,         // Unique identifier for the artist
+    artistId: number,         // Unique identifier for the artist
     songId: number,           // Unique identifier for the song
     mode: number,             // Mode of the song (0 = minor, 1 = major)
-    Danceability: number,     // Danceability score of the song (a number between 0.0 and 1.0)
+    danceability: number,     // Danceability score of the song (a number between 0.0 and 1.0)
     energy: number,           // Energy score of the song (a number between 0.0 and 1.0)
     loudness: number,         // Loudness of the song (in dB, typically between -60 and 0)
-    Speechiness: number,      // Speechiness score of the song (a number between 0.0 and 1.0)
-    Acousticness: number,     // Acousticness score of the song (a number between 0.0 and 1.0)
-    Instrumentalness: number, // Instrumentalness score of the song (a number between 0.0 and 1.0)
-    Valence: number,          // Valence score of the song (a number between 0.0 and 1.0, represents the musical "positiveness")
+    speechiness: number,      // Speechiness score of the song (a number between 0.0 and 1.0)
+    acousticness: number,     // Acousticness score of the song (a number between 0.0 and 1.0)
+    instrumentalness: number, // Instrumentalness score of the song (a number between 0.0 and 1.0)
+    valence: number,          // Valence score of the song (a number between 0.0 and 1.0, represents the musical "positiveness")
     tempo: number,            // Tempo of the song in beats per minute (BPM)
-    Liveness: number          // Liveness score of the song (a number between 0.0 and 1.0, represents how "live" the song feels)
+    liveness: number          // Liveness score of the song (a number between 0.0 and 1.0, represents how "live" the song feels)
   }
   
 
@@ -25,18 +25,18 @@
     {
       key: 8,
       genre: ["rock", "indie"],
-      ArtistId: 54321,
+      artistId: 54321,
       songId: 98765,
       mode: 0,
-      Danceability: 0.55,
+      danceability: 0.55,
       energy: 0.70,
       loudness: -8,
-      Speechiness: 0.10,
-      Acousticness: 0.20,
-      Instrumentalness: 0.05,
-      Valence: 0.40,
+      speechiness: 0.10,
+      acousticness: 0.20,
+      instrumentalness: 0.05,
+      valence: 0.40,
       tempo: 130,
-      Liveness: 0.50
+      liveness: 0.50
     },
     // 9 more objects with the same structure
   ];
